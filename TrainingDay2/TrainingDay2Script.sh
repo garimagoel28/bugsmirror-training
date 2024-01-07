0c203580f9264e551b43f623be211fb0b9b70f48
garimagoel28 <garima.kkr1@gmail.com>
2024-01-07 17:05:08 +0530

    Day 2 task: download subtask added, directory creation subtask improved

--- TrainingDay2/TrainingDay2Script.sh
+++ TrainingDay2/TrainingDay2Script.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
 # Task 1: Create a directory named 'TrainingDay2'
-mkdir TrainingDay2
+directory_name="TrainingDay2"
 
-# Task 2: Download files from the drive and save them to the folder
+# Check if the directory already exists
+if [ -d "${directory_name}" ]; then
+    echo "Directory '${directory_name}' already exists. Skipping creation."
+else
+    # If the directory doesn't exist, proceed with the creation
+    mkdir "${directory_name}"
+
+    # Check if the creation was successful
+    if [ $? -eq 0 ]; then
+        echo "Directory '${directory_name}' created successfully!"
+    else
+        echo "Failed to create directory."
+    fi
+fi
+
+
+# Task 2: Download files from the drive and save them to the folder 'TrainingDay2'
+file_id="1Xe5etJN_YXHBcWDhaRed5EbNYpyWr8hL"
+downloaded_file="TrainingDay2/sample.txt"
+
+gdown "https://drive.google.com/uc?id=${file_id}" -O "${downloaded_file}"
 
 # Task 3: Retrieve and put system information into a text file
-# Creating a file named 'system_info.txt' in the 'TrainingDay2' folder
+# Creating a file named 'system_info.txt' in the 'TrainingDay2 folder'
 output_file="TrainingDay2/system_info.txt"
 
 # Retrieving system information and appending it to the text file
